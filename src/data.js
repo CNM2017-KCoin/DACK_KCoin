28215e456f91158f5da79501367fe69805e6eda3
DESKTOP-P148DK7\VuQuang <vuquangkhtn@gmail.com>
2018-01-07 09:40:34 +0700

    add data for test
    add data for test

--- src/data.js
+++ src/data.js
@@ -8,32 +8,38 @@ const data = {
     address:"f25921e2aa64e494ff00b5ab09",
     actual_amount: 100000,
     available_amount: 10000,
+    role: "User",
     receiverTrans: [
         {
+            timestamp: 123456789,
             referencedOutputHash:"ce91809664e549f740c38fc6d",
             referencedOutputIndex: 0,
             amount: 10000,
             sender_address:"a5f720c8080d81b9bd9781bf85c38c4d2"
         },
         {
+            timestamp: 123456789,
             referencedOutputHash:"ce91809664e549f740c38fc6d",
             referencedOutputIndex: 1,
             amount: 10000,
             sender_address:"a5f720c8080d81b9bd9781bf85c38c4d2"
         },
         {
+            timestamp: 123456789,
             referencedOutputHash:"ce91809664e549f740c38fc6d",
             referencedOutputIndex: 2,
             amount: 10000,
             sender_address:"a5f720c8080d81b9bd9781bf85c38c4d2"
         },
         {
+            timestamp: 123456789,
             referencedOutputHash:"ce91809664e549f740c38fc6d",
             referencedOutputIndex: 3,
             amount: 10000,
             sender_address:"a5f720c8080d81b9bd9781bf85c38c4d2"
         },
         {
+            timestamp: 123456789,
             referencedOutputHash:"ce91809664e549f740c38fc6d",
             referencedOutputIndex: 4,
             amount: 10000,
@@ -42,36 +48,42 @@ const data = {
     ],
     senderTrans: [
         {
+            timestamp: 123456789,
             amount: 10000,
             status: "sucess",
             receiver_address:"a5f720c8080d81b9bd9781bf85c38c4d2"
  
         },
         {
+            timestamp: 123456789,
             amount: 10000,
             status: "waiting",
             receiver_address:"a5f720c8080d81b9bd9781bf85c38c4d2"
  
         },
         {
+            timestamp: 123456789,
             amount: 10000,
             status: "fail",
             receiver_address:"a5f720c8080d81b9bd9781bf85c38c4d2"
  
         },
         {
+            timestamp: 123456789,
             amount: 10000,
             status: "sucess",
             receiver_address:"a5f720c8080d81b9bd9781bf85c38c4d2"
  
         },
         {
+            timestamp: 123456789,
             amount: 10000,
             status: "waiting",
             receiver_address:"a5f720c8080d81b9bd9781bf85c38c4d2"
  
         },
         {
+            timestamp: 123456789,
             amount: 10000,
             status: "fail",
             receiver_address:"a5f720c8080d81b9bd9781bf85c38c4d2"
