f6ec345d496e876bd8e4d4cfefcaf53e77231443
DESKTOP-P148DK7\VuQuang <vuquangkhtn@gmail.com>
2018-01-10 01:11:08 +0700

    fix event in send trans
    fix event in send trans

--- src/containers/TransactionDetailPage.js
+++ src/containers/TransactionDetailPage.js
@@ -13,13 +13,17 @@ import Data from '../data';
 import {Tabs, Tab} from 'material-ui/Tabs';
 import Slider from 'material-ui/Slider';
 import IconButton from 'material-ui/IconButton';
-import LeftIco from 'material-ui/svg-icons/hardware/keyboard-arrow-left';
-import RightIco from 'material-ui/svg-icons/hardware/keyboard-arrow-right';
+import ActiveIco from 'material-ui/svg-icons/action/done';
+import CancelIco from 'material-ui/svg-icons/content/clear';
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
 import TextField from 'material-ui/TextField';
 import Pager from 'react-pager';
 import {connect} from 'react-redux';
 // import * as actions from './../actions/index.js';
 import globalStyles from '../styles';
+
+const Utils = require('../services/utils');
 
 class TransactionDetailPage extends React.Component {
   constructor(props) {
@@ -32,6 +36,7 @@ class TransactionDetailPage extends React.Component {
       this.state = {
         email: email,
         role: role,
+        currentItem: {},
         receiver_trans: [],
         sender_trans: [],
         showCheckboxes: false,
@@ -43,7 +48,10 @@ class TransactionDetailPage extends React.Component {
         receiverVisiblePage: 1,
         isReceiverTab: true, //send,receive
         senderReport: 'Đang xử lý...',
-        receiverReport: 'Đang xử lý...'
+        receiverReport: 'Đang xử lý...',
+        errorTxtVertifyPass: '',
+        errorTxtCode: '',
+        open:false
       };
   }
   
@@ -102,8 +110,7 @@ class TransactionDetailPage extends React.Component {
       return;
     })
     .catch(function (error) {
-      console.log(error);
-      alert('Load users failed', error);
+      alert('Load users failed');
       return;
     });
   }
@@ -137,8 +144,7 @@ class TransactionDetailPage extends React.Component {
       return;
     })
     .catch(function (error) {
-      console.log(error);
-      alert('Load users failed', error);
+      alert('Load users failed');
       return;
     });
   }
@@ -158,6 +164,26 @@ class TransactionDetailPage extends React.Component {
     });
   };
 
+  onDialogChange(event, newValue) {
+    if (event.target.value == '') {
+      switch(event.target.id) {
+        case 'vertify_pass': {
+          this.setState({ errorTxtVertifyPass: 'Vertify Password is required' });
+          return;
+        }
+        case 'code': {
+          this.setState({ errorTxtCode: 'Code is required' });
+          return;
+        }
+      }
+    } else {
+      this.setState ({ 
+        errorTxtVertifyPass: '', 
+        errorTxtCode: ''
+      });
+    }
+  }
+
   componentWillReceiveProps() {
     if(this.state.isReceiverTab) {
       this.loadReceiverData(this.state.receiverCurrent)
@@ -172,6 +198,109 @@ class TransactionDetailPage extends React.Component {
     } else {
       this.loadSenderData(this.state.senderCurrent);
     };
+  }
+
+  postTransaction = (event) => {
+    const txtVertifyPass = document.getElementById('vertify_pass');
+    const txtCode = document.getElementById('code');
+
+    if(txtVertifyPass.value == "") {
+      this.setState({ errorTxtVertifyPass: 'Vertify Password is required' })
+      return;
+    }
+    if(txtCode.value == "") {
+      this.setState({ errorTxtCode: 'Code is required' })
+      return;
+    }
+
+    var self = this;
+
+    var passwordHash = Utils.hash(txtVertifyPass.value).toString('hex');
+    //post transaction
+    const apiLink = 'https://api-dack-kcoin-wantien.herokuapp.com';
+    axios.post(apiLink+'/api/send', {
+      code: txtCode.value,
+      password: passwordHash,
+      email: self.state.email,
+      receiver_address: self.state.currentItem.receiver_address,
+      amount: self.state.currentItem.amount,
+      transaction_id: self.state.currentItem.transaction_id
+    })
+    .then(function (response) {
+      console.log(response);
+      if(response.data.status == 200){
+        alert('Gửi giao dịch thành công!');
+        browserHistory.push('/transactions');
+      } else if(response.status == 400) {
+        alert('Số dư trong tài khoản không đủ để thực hiện giao dịch');
+      } else {
+        alert('Giao dịch không thành công');
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+      alert('Giao dịch thất bại');
+    });
+
+    this.hideDialog();
+  };
+
+  hideDialog() {
+    this.setState({open: false});
+    browserHistory.push('/transactions');
+  }
+
+  handleActiveClick(event, item) {
+    console.log('active');
+    var self = this;
+    //send vertify request
+    const apiLink = 'https://api-dack-kcoin-wantien.herokuapp.com';
+    axios.post(apiLink+'/api/send-validate', {
+      email: self.state.email
+    })
+    .then(function (response) {
+      console.log(response);
+      if(response.data.status == 200){
+        alert('Đã gửi xác nhận thành công!');      
+        self.setState({
+          currentItem: item,
+          open: true
+        });
+      } else {
+        alert('Gửi xác nhận thất bại');
+        return;
+      }
+
+    })
+    .catch(function (error) {
+      console.log(error);
+      alert('Xác nhận thất bại');
+    });
+  }
+
+  handleCancelClick(event, item) {
+    console.log('cancel');
+    var self = this;
+    //send vertify request
+    const apiLink = 'https://api-dack-kcoin-wantien.herokuapp.com';
+    axios.post(apiLink+'/api/cancel-transaction', {
+      email: self.state.email,
+      transaction_id: item.transaction_id
+    })
+    .then(function (response) {
+      console.log(response);
+      if(response.data.status == 200){
+        alert('Đã hủy giao dịch thành công!');
+      } else {
+        alert('Hủy giao dịch thất bại');
+      }
+      return;
+
+    })
+    .catch(function (error) {
+      console.log(error);
+      alert('Yêu cầu thất bại');
+    });
   }
 
   render() {
@@ -215,30 +344,106 @@ class TransactionDetailPage extends React.Component {
           width: '20%'
         },
         address: {
-          width: '30%'
+          width: '35%'
         },
         amount: {
-          width: '25%'
+          width: '15%'
+        },
+        status_creating: {
+          color: 'blue',
+          fontWeight: 'bold',
+          width: '15%'
         },
         status_success: {
           color: 'green',
           fontWeight: 'bold',
-          width: '25%'
+          width: '15%'
         },
         status_fail: {
           color: 'red',
           fontWeight: 'bold',
-          width: '25%'
+          width: '15%'
         },
         status_waiting: {
           fontWeight: 'bold',
-          width: '25%'
+          width: '15%'
         },
         status: {
-          width: '25%'
+          width: '15%'
+        },
+        button: {
+          width: '15%'
         }
+      },
+      buttons: {
+        marginTop: 30,
+        float: 'right'
+      },
+      saveButton: {
+        marginLeft: 5
       }
     };
+
+
+    var context =  (
+      <form>
+        <TextField
+          id="vertify_pass"
+          hintText="Vertify Password"
+          floatingLabelText="Vertify Password"
+          fullWidth={true}
+          type="password"
+          errorText= {this.state.errorTxtVertifyPass}
+          onChange={this.onDialogChange.bind(this)}
+        />
+
+        <TextField
+          id="code"
+          hintText="Code"
+          floatingLabelText="Code"
+          fullWidth={true}
+          errorText= {this.state.errorTxtCode}
+          onChange={this.onDialogChange.bind(this)}
+        />
+
+        <Divider/>
+
+        <div style={styles.buttons}>
+          <Link to="/transactions">
+            <RaisedButton label="Thoát" 
+            onClick={(event) => this.hideDialog()}/>
+          </Link>
+
+          <RaisedButton label="Gửi"
+            style={styles.saveButton}
+            onClick={(event) => this.postTransaction(event)}
+            primary={true}/>
+        </div>
+      </form>
+    );
+
+
+    let showButton = (item) => {
+      if(item.status == 'creating') {
+        return (
+          <div>
+
+            <IconButton onClick={(event) => this.handleActiveClick(event, item)} >
+              <ActiveIco />
+            </IconButton>
+
+            <IconButton onClick={(event) => this.handleCancelClick(event, item)}>
+              <CancelIco />
+            </IconButton>
+          </div>
+        );
+      } else {
+        return (
+          <div>
+          </div>
+        );
+      }
+    }
 
     var transactions = [];
     if(this.state.isReceiverTab) {
@@ -254,9 +459,19 @@ class TransactionDetailPage extends React.Component {
           <Tabs onChange={this.handleTabChange}>
             <Tab label="Nhận tiền" >
               <h3>{this.state.receiverReport}</h3>
+              <Link to="/addTransaction" >
+                      <FloatingActionButton style={styles.floatingActionButton} backgroundColor={pink500}>
+                        <ContentAdd />
+                      </FloatingActionButton>
+                    </Link>
             </Tab>
             <Tab label="Gửi tiền" >
               <h3>{this.state.senderReport}</h3>
+              <Link to="/addTransaction" >
+                      <FloatingActionButton style={styles.floatingActionButton} backgroundColor={pink500}>
+                        <ContentAdd />
+                      </FloatingActionButton>
+                    </Link>
             </Tab>
           </Tabs>
         </div>
@@ -293,9 +508,9 @@ class TransactionDetailPage extends React.Component {
                         {transactions.map(item =>
                           <TableRow key={item.transaction_id}>
                           <TableRowColumn style={styles.columnsReceiverTable.timestamp}>{item.timestamp}</TableRowColumn>
-                            <TableRowColumn style={styles.columnsReceiverTable.address}>{item.sender_address}</TableRowColumn>
-                            <TableRowColumn style={styles.columnsReceiverTable.hash}>{item.ref_hash}</TableRowColumn>
-                            <TableRowColumn style={styles.columnsReceiverTable.index}>{item.ref_index}</TableRowColumn>
+                            <TableRowColumn style={styles.columnsReceiverTable.address}>{item.sender_address==-1?'null':item.sender_address}</TableRowColumn>
+                            <TableRowColumn style={styles.columnsReceiverTable.hash}>{item.ref_hash==-1?'null':item.ref_hash}</TableRowColumn>
+                            <TableRowColumn style={styles.columnsReceiverTable.index}>{item.ref_index==-1?'null':item.ref_index}</TableRowColumn>
                             <TableRowColumn style={styles.columnsReceiverTable.amount}>{item.amount}</TableRowColumn>
                           </TableRow>
                         )}
@@ -338,6 +553,7 @@ class TransactionDetailPage extends React.Component {
                           <TableHeaderColumn style={styles.columnsSenderTable.address}>Receiver Address</TableHeaderColumn>
                           <TableHeaderColumn style={styles.columnsSenderTable.amount}>Amount</TableHeaderColumn>
                           <TableHeaderColumn style={styles.columnsSenderTable.status}>status</TableHeaderColumn>
+                          <TableHeaderColumn style={styles.columnsSenderTable.button}></TableHeaderColumn>
                         </TableRow>
                       </TableHeader>
                       <TableBody displayRowCheckbox={this.state.showCheckboxes}>
@@ -345,6 +561,11 @@ class TransactionDetailPage extends React.Component {
                           var statusStyle = {};
                           var statusText = '';
                           switch(item.status) {
+                            case 'creating': {
+                              statusStyle = styles.columnsSenderTable.status_creating;
+                              statusText = "Đã khởi tạo";
+                              break;
+                            }
                             case 'waiting': {
                               statusStyle = styles.columnsSenderTable.status_waiting;
                               statusText = "Đang xử lý";
@@ -352,7 +573,7 @@ class TransactionDetailPage extends React.Component {
                             }
                             case 'fail': {
                               statusStyle = styles.columnsSenderTable.status_fail;
-                              statusText = "Thất bại";
+                              statusText = "Đã hủy";
                               break;
                             }
                             case 'success': {
@@ -364,10 +585,11 @@ class TransactionDetailPage extends React.Component {
                           // console.log(status_style);
                           return(
                             <TableRow key={item.transaction_id}>
-                            <TableRowColumn style={styles.columnsSenderTable.timestamp}>{item.timestamp}</TableRowColumn>
-                            <TableRowColumn style={styles.columnsSenderTable.address}>{item.receiver_address}</TableRowColumn>
-                            <TableRowColumn style={styles.columnsSenderTable.amount}>{item.amount}</TableRowColumn>
-                            <TableRowColumn style={statusStyle}>{statusText}</TableRowColumn>                
+                              <TableRowColumn style={styles.columnsSenderTable.timestamp}>{item.timestamp}</TableRowColumn>
+                              <TableRowColumn style={styles.columnsSenderTable.address}>{item.receiver_address}</TableRowColumn>
+                              <TableRowColumn style={styles.columnsSenderTable.amount}>{item.amount}</TableRowColumn>
+                              <TableRowColumn style={statusStyle}>{statusText}</TableRowColumn>   
+                              <TableRowColumn style={styles.columnsSenderTable.button}>{showButton(item)}</TableRowColumn>          
                             </TableRow>                         
                           )}
                         )}
@@ -390,6 +612,14 @@ class TransactionDetailPage extends React.Component {
               </div>
             </Tab>
           </Tabs>
+                  
+          <Dialog
+            title="Xác nhận giao dịch"
+            modal={true}
+            open={this.state.open}>
+              <div>Vui lòng kiểm tra email để lấy mã xác nhận cho giao dịch</div>
+              {context}
+          </Dialog>
         </div>
       );
     }
